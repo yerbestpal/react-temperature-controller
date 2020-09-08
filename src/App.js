0c341d8deb19a4adbe356a44fd5219066b4946fe
Ross McLean <rmclean92@pm.me>
2020-09-08 22:59:15 +0100

    Update App.js - change inline functions to function references

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
 
 const App = () => {
+  // States
   const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState('cold');
+
+  const increaseTemperature = () => {
+    setTemperatureValue(temperatureValue + 1);
+  }
+
+  const decreaseTemperature = () => {
+    setTemperatureValue(temperatureValue - 1);
+  }
+
   return (
     <div className='app-container'>
       <div className='temperature-display-container'>
-  <div className='temperature-display'>{temperatureValue}°C</div>
+  <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°C</div>
       </div>
       <div className='button-container'>
-        <button onClick={() => setTemperatureValue(temperatureValue + 1)}>+</button>
-        <button onClick={() => setTemperatureValue(temperatureValue - 1)}>-</button>
+        <button onClick={increaseTemperature}>+</button>
+        <button onClick={decreaseTemperature}>-</button>
       </div>
     </div>
   );
