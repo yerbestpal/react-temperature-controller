729dca2162abcae37786219f8c581ba703daa386
Ross McLean <rmclean92@pm.me>
2020-09-08 21:59:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <div className='temperature-display-container'>
+        <div className='temperature-display'>10°C</div>
+      </div>
+      <div className='button-container'>
+        <button>+</button>
+        <button>-</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
